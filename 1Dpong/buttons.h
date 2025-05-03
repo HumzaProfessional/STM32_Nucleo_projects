22854216f956d248f37b02ed3fed891527551a4a
Humza Rana <142959387+HumzaProfessional@users.noreply.github.com>
2025-05-03 01:49:46 -0400

    Update buttons.h

--- 1Dpong/buttons.h
+++ 1Dpong/buttons.h
@@ -1,10 +1,15 @@
-#ifndef BUTTON_H
-#define BUTTON_H
-
-
+#ifndef BUTTONS_H
+#define BUTTONS_H
 
 #include "stm32l476xx.h"
 
+// Definitions that organize the button data positions
+#define NUM_BUTTONS 3
+#define BTN_RIGHT   0
+#define BTN_LEFT    1
+#define BTN_USER    2
+
+// structure which stores all the buttons
 typedef struct {
     uint32_t filter;
     uint32_t state;
@@ -12,14 +17,10 @@ typedef struct {
     uint32_t pin;
 } Button;
 
-#define NUM_BUTTONS 3
-
-#define BTN_RIGHT   0
-#define BTN_LEFT    1
-#define BTN_USER    2
-
+// Allows global access to the array
 extern volatile Button buttons[NUM_BUTTONS];
 
+// function for initializing buttons
 void init_Buttons(void);
 
 #endif
